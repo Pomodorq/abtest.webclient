7b943119d580804159e05d1883d16c3ef8bd1659
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-12 23:54:16 +0700

    Create overrides.ts

---
+++ src/theme/overrides.ts
@@ -0,0 +1,15 @@
+import MuiAppBar from 'theme/appbar';
+
+const overrides = {
+  // Style sheet name ⚛️
+  MuiButton: {
+    // Name of the rule
+    text: {
+      // Some CSS
+      color: 'white',
+    },
+  },
+  MuiAppBar,
+};
+
+export default overrides;
