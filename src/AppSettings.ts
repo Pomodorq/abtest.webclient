76c97d49af835d71f95e8fbf250136055df28f46
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-17 00:43:22 +0700

    Update AppSettings.ts

--- src/AppSettings.ts
+++ src/AppSettings.ts
@@ -1,4 +1,6 @@
 export const server =
-  process.env.REACT_APP_ENV === 'production' ? '' : 'http://localhost:6178';
+  process.env.REACT_APP_ENV === 'production'
+    ? 'https://abtest-backend.azurewebsites.net'
+    : 'http://localhost:6178';
 
 export const webAPIUrl = `${server}/api`;
